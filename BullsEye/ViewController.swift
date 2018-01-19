59c5bcd2c6cb5fb56e3da1a89cdc3b2ae996c5cd
Oval Frame <ovalframe@Ovals-MacBook-Pro.local>
2018-01-19 12:10:49 -0600

    Complete UI Layout

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,9 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    var score: Int = 0
+    var round: Int = 0
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        currentValue = lroundf(slider.value)
+        
+        startNewRound()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +36,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewRound(){
+        
+        //reset the slider to the "home" position
+        currentValue = 50
+        slider.value = Float(currentValue)
+        
+        //generate a new target
+        targetValue = 1 + Int(arc4random_uniform(100))
+        
+        //increment the round
+        round = round + 1
+        
+        updateLabels()
+    }
+    
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        currentValue = lroundf(slider.value)
+        print("The value of the slider is \(currentValue)")
+    }
 
+    @IBAction func showAlert(){
+        let message = "The value of the current slider is: \(currentValue)"
+        
+        let alert = UIAlertController(title: "alert title", message: message, preferredStyle: .alert)
+   
+        let action = UIAlertAction(title: "action title", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
 
 }
-
